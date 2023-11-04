b050d83b65d1833ac754925da8b3d66e819ecd4a
BVolt <Brendenj7@hotmail.com>
2023-11-03 23:09:58 -0700

    output changes

--- src/main/java/com/mycompany/nonlinearroots/NonlinearRoots.java
+++ src/main/java/com/mycompany/nonlinearroots/NonlinearRoots.java
@@ -1,20 +1,14 @@
 package com.mycompany.nonlinearroots;
 import java.io.*;
-import java.util.*;
 
-/* Future tasks
-Graph each original function
-Graph percent error y-axis compared to number of iterations
--All four methods on one graph^
--Y axis needs to be logarithmic
+/* Tasks
+-Get A B Graphs
+-Write divergence test
+-Report on [a, b], xN, [xN, xN-1] starting points, convergence of the root, observation, and data types
+-Grab table snaps
+-Get Graphs for tables
 
-divergence test
-
-How are intervals determined [a, b], xN, [xN, xN-1]
-Write report showing print out table of values along with graphs
--Discuss starting points and convergence of the root
--Discuss strange observation
--Discuss data ypes used
+-Report Order Code -> Discussion -> AB Graphs -> Root Discussion -> Table Graph -> Tables
  */
 
 public class NonlinearRoots {
@@ -25,50 +19,35 @@ public class NonlinearRoots {
         double eval(double x);
     }
 
-    public static void main(String[] args) {
-        NonlinearRoots p3 = new NonlinearRoots();
-        ArrayList<Double> rootsA = new ArrayList<>();
-        ArrayList<Double> rootsB = new ArrayList<>();
-        
+    public static void main(String[] args) {    
         //Hard Coded function to be passed to our methods
         Function A = (double x) -> 2 * Math.pow(x, 3) - 11.7 * Math.pow(x, 2) + 17.7 * x - 5;
         Function B = (double x) -> x + 10 - x * Math.cosh(50/x);
         Function Aprime = (double x) -> 6 * Math.pow(x, 2) - 23.4 * x + 17.7;
         Function Bprime = (double x) ->(50 * Math.sinh(50/x)/x)- Math.cosh(50/x) + 1;
         
-        
-        
-        //Bisection Method Function A
+        //Function A - Root 1
         applyMethod("Bisection", "A", 1, A, 0, 1, 0.365, 0, 0, A);
-        applyMethod("Bisection", "A", 2, A, 1, 2, 1.922, 0, 0, A);
-        applyMethod("Bisection", "A", 3, A, 3, 4, 3.563, 0, 0, A);
-        
-        //Bisection Method Function B
-        applyMethod("Bisection", "B", 1, B, 120, 130, 126.632, 0, 0, A);
-        
-//        
-        //Falsi Method Function A
         applyMethod("Falsi", "A", 1, A, 0, 1, 0.365, 0, 0, A);
-        applyMethod("Falsi", "A", 2, A, 1, 2, 1.922, 0, 0, A);
-        applyMethod("Falsi", "A", 3, A, 3, 4, 3.563, 0, 0, A);
-        
-        //Falsi Method Function B
-        applyMethod("Falsi", "B", 1, B, 120, 130, 126.632, 0, 0, B);
-        
-        //Newton Method Function A
         applyMethod("Newton", "A", 1, A, 0, 1, 0.365, 1, 0, Aprime);
-        applyMethod("Newton", "A", 2, A, 1, 2, 1.922, 2, 0, Aprime);
-        applyMethod("Newton", "A", 3, A, 3, 4, 3.563, 4, 0, Aprime);
-        
-        //Newton Method Function B
-        applyMethod("Newton", "B", 1, B, 120, 130, 126.632, 130, 0, Bprime);
-
-        //Newton Method Function A
         applyMethod("Secant", "A", 1, A, 0, 1, 0.365, 0, 1, Aprime);
+
+        //Function A - Root 2
+        applyMethod("Bisection", "A", 2, A, 1, 2, 1.922, 0, 0, A);
+        applyMethod("Falsi", "A", 2, A, 1, 2, 1.922, 0, 0, A);
+        applyMethod("Newton", "A", 2, A, 1, 2, 1.922, 2, 0, Aprime);
         applyMethod("Secant", "A", 2, A, 1, 2, 1.922, 1, 2, Aprime);
+
+        //Function A - Root 3
+        applyMethod("Bisection", "A", 3, A, 3, 4, 3.563, 0, 0, A);
+        applyMethod("Falsi", "A", 3, A, 3, 4, 3.563, 0, 0, A);
+        applyMethod("Newton", "A", 3, A, 3, 4, 3.563, 4, 0, Aprime);
         applyMethod("Secant", "A", 3, A, 3, 4, 3.563, 3, 4, Aprime);
         
-        //Newton Method Function B
+        //Function B - Root 1
+        applyMethod("Bisection", "B", 1, B, 120, 130, 126.632, 0, 0, A);
+        applyMethod("Falsi", "B", 1, B, 120, 130, 126.632, 0, 0, B);
+        applyMethod("Newton", "B", 1, B, 120, 130, 126.632, 130, 0, Bprime);
         applyMethod("Secant", "B", 1, B, 120, 130, 126.632, 127, 130, Bprime);
     }
     
@@ -124,7 +103,6 @@ public class NonlinearRoots {
                 b = c;
             else if (cXb < 0)
                 a = c;
-            //
             else{
             }
             //End falsi if error is less than 1 percent
@@ -143,10 +121,10 @@ public class NonlinearRoots {
         openFile(outFile);
         for(int i = 0; i < 100; i++){
             xNp1 = xN - func.eval(xN)/funcPrime.eval(xN);
-            xN = xNp1;
             error = findError(accurate,xNp1);
             outputErr(error);
             System.out.printf("%-15d%-15f%-15f%-15f%-15f%-15f%-15f\n", i+1, xN, func.eval(xN), funcPrime.eval(xN), func.eval(xN)/funcPrime.eval(xN), xNp1, error);
+            xN = xNp1;
             //End Newton method if less than 1 percent
             if(error < .01)
                 break;
@@ -162,12 +140,12 @@ public class NonlinearRoots {
         openFile(outFile);
         for(int i = 0; i < 100; i++){
             xNp1 = xN - ((xN - xNm1)/(func.eval(xN)-func.eval(xNm1))) * func.eval(xN);
+            error = findError(accurate,xNp1);
+            outputErr(error);
+            System.out.printf("%-15d%-15f%-15f%-15f%-15f%-15f%-15f\n", i+1, xN,xNm1, func.eval(xN), func.eval(xN-1), xNp1, error);
             xNm1 = xN;
             xN = xNp1;
-            error = findError(accurate,xNp1);
-            System.out.printf("%-15d%-15f%-15f%-15f%-15f%-15f%-15f\n", i+1, xN,xNm1, func.eval(xN), func.eval(xN-1), xNp1, error);
-            outputErr(error);
-            //End Newton method if less than 1 percent
+            //End Secant method if less than 1 percent
             if(error < .01)
                 break;
         }
